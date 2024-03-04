9720c52a1ecf5b353fb15d152386a106be787c37
mattavireddy <mattavireddycoder@outlook.com>
2024-03-04 13:50:28 +0530

    added prettier for tailwindcss sorting

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ function App() {
       <div>
         <Button>Click me</Button>
       </div>
+
+      <div className="ml-4 flex h-24 border-2 border-gray-300 p-3 text-gray-700 shadow-md"></div>
     </>
   );
 }
